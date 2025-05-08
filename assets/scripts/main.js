e78a358729c0a67f138eeb2852f19442488c0152
crrrr30 <jonathancui03@gmail.com>
2025-05-07 19:37:45 -0700

    explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -60,13 +60,14 @@ function addRecipesToDocument(recipes) {
 /**
  * Takes in an array of recipes, converts it to a string, and then
  * saves that string to 'recipes' in localStorage
- * @param {Array<Object>} recipes An array of recipes
+ * @param {Array<RecipeCardData>} recipes An array of recipes
  */
 function saveRecipesToStorage(recipes) {
 	// EXPLORE - START (All explore numbers start with B)
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
+	localStorage.setItem("recipes", JSON.stringify(recipes));
 }
 
 /**
@@ -74,22 +75,59 @@ function saveRecipesToStorage(recipes) {
  * <button>.
  */
 function initFormHandler() {
+	const main = document.getElementsByTagName("main")[0];
+
 	// B2. TODO - Get a reference to the <form> element
+	const form = document.getElementsByTagName("form")[0];
+
 	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
 	//            submit button is clicked
-	// Steps B4-B9 will occur inside the event listener from step B3
-	// B4. TODO - Create a new FormData object from the <form> element reference above
-	// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
-	//            make this easier to read), and then extract the keys and corresponding
-	//            values from the FormData object and insert them into recipeObject
-	// B6. TODO - Create a new <recipe-card> element
-	// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
-	// B8. TODO - Append this new <recipe-card> to <main>
-	// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
-	//            then save the recipes array back to localStorage
-	// B10. TODO - Get a reference to the "Clear Local Storage" button
-	// B11. TODO - Add a click event listener to clear local storage button
-	// Steps B12 & B13 will occur inside the event listener from step B11
-	// B12. TODO - Clear the local storage
-	// B13. TODO - Delete the contents of <main>
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+
+		alert("submitted");
+
+		// Steps B4-B9 will occur inside the event listener from step B3
+		// B4. TODO - Create a new FormData object from the <form> element reference above
+		const formData = Object.fromEntries(new FormData(form));
+
+		// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
+		//            make this easier to read), and then extract the keys and corresponding
+		//            values from the FormData object and insert them into recipeObject
+		/** @type {RecipeCardData} */
+		// @ts-ignore
+		const recipeObject = {};
+		for (const key in formData) {
+			recipeObject[key] = formData[key];
+		}
+
+		// B6. TODO - Create a new <recipe-card> element
+		const card = new RecipeCard();
+
+		// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
+		// @ts-ignore
+		card.data = recipeObject;
+
+		// B8. TODO - Append this new <recipe-card> to <main>
+		main.appendChild(card);
+
+		// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
+		//            then save the recipes array back to localStorage
+		saveRecipesToStorage([...getRecipesFromStorage(), recipeObject]);
+
+		// B10. TODO - Get a reference to the "Clear Local Storage" button
+		const clearLocalStorageButton = [...document.getElementsByTagName("button")].filter(
+			button => button.textContent === "Clear Local Storage"
+		)[0];
+
+		// B11. TODO - Add a click event listener to clear local storage button
+		clearLocalStorageButton.click = () => {
+			// Steps B12 & B13 will occur inside the event listener from step B11
+			// B12. TODO - Clear the local storage
+			saveRecipesToStorage([]);
+
+			// B13. TODO - Delete the contents of <main>
+			main.innerHTML = "";
+		}
+	});
 }
